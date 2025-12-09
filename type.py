5c25228d89d2972295fe4935963c3641b724d3f9
sho190450 <sarun.mayai19@gmail.com>
2025-12-09 11:51:43 +0700

    Add files via upload

---
+++ type.py
@@ -0,0 +1,21 @@
+FullName = ("Napintor")
+IDStu = 684245006
+Gpax = 3.25
+Age = 18
+
+if(type(FullName) == str):
+    print("Napintor is Str")
+else:
+    print("Napintor is not str")
+if(type(IDStu) == complex):
+    print("684245006 is complex")
+else:
+    print("684245006 is not complex")
+if(type(Gpax) == float):
+    print("3.25 is float")
+else:
+    print("3.25 is not float")
+if(type(Age) == int):
+    print("18 is int")
+else:
+    print("18 is not int")
